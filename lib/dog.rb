26a90393fbcd1b44bdcc7157a0afebb9df261aa5
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-13 13:59:08 -0700

    commit tree ff6d620e4da60a64f5f5f139030441dcb6ab2028

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,15 +42,33 @@ class Dog
     end
 
     def self.new_from_db(row)
-        dog = Dog.new(name:row[1], breed:row[2])
-        dog.save
-        dog
+        self.new(id:row[0], name:row[1], breed:row[2])
     end
 
     def self.all
         sql = <<-SQL
         SELECT * FROM dogs
         SQL
-        DB[:conn].execute(sql)
+        DB[:conn].execute(sql).map do |row|
+            self.new_from_db(row)
+        end
     end
+
+    def self.find_by_name(name)
+        sql = <<-SQL
+        SELECT * FROM dogs WHERE NAME = ?
+        SQL
+        dog = DB[:conn].execute(sql, name)
+        self.new_from_db(dog[0])
+    end
+
+    def self.find(id)
+        sql = <<-SQL
+        SELECT * FROM dogs WHERE ID = ?
+        SQL
+        dog = DB[:conn].execute(sql, id)
+        self.new_from_db(dog[0])
+    end
+
+
 end
